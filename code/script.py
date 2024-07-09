499b03c1bff9da3c0653dd1529725a2d83264b2e
MNK-FTHR <victorgranier.pierre@gmail.com>
2024-07-09 16:34:13 +0200

    first filters easy

--- code/script.py
+++ code/script.py
@@ -18,12 +18,24 @@ def addSerieAndVieToData():
     for i, data in enumerate(sorted_by_sessionid):
         data['Serie'] = "0"
     betterDataList.extend(sorted_by_sessionid)
-    # session_ids = set()
-    # for d in betterDataList:
-    #     session_ids.add(d['SessionID'])
-    # unique_session_ids = list(session_ids)
-    # print([d for d in betterDataList if d['SessionID'] == unique_session_ids[0]])
-    # for id in unique_session_ids:
+    session_ids = set()
+    for d in betterDataList:
+        session_ids.add(d['SessionID'])
+    unique_session_ids = list(session_ids)
+    for id in unique_session_ids:
+        oneTrueTrueIncrement = 0
+        for item in [d for d in betterDataList if d['SessionID'] == id]:
+            life = 2
+            # 2 TRUE TRUE
+            if ((item['Niveau'] == '2') and (item["Assis"]=="True") and (item['Allonge']=="True")):
+                item['Serie'] = str(int(item['Serie'])+1)
+            # 1 TRUE TRUE && 1 TRUE TRUE
+            if ((item['Niveau'] == '1') and (item["Assis"]=="True") and (item['Allonge']=="True")):
+                if oneTrueTrueIncrement >0:
+                    item['Serie'] = str(int(item['Serie'])+1)
+                elif oneTrueTrueIncrement == 0:
+                    oneTrueTrueIncrement += 1
+    print(betterDataList)
 
 
 if os.path.isfile('smallerEnregistrement.csv'):
@@ -35,11 +47,8 @@ if os.path.isfile('smallerEnregistrement.csv'):
     with open(fileToWrite, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerow(fields)
-        session_ids = set()
-        for d in betterDataList:
-            session_ids.add(d['SessionID'])
-            unique_session_ids = list(session_ids)
-            writer.writerow(list([d for d in betterDataList if d['SessionID'] == unique_session_ids[0]].values()))
-        # for data in betterDataList:
+        for lines in betterDataList:
+            writer.writerow(list(lines.values()))
+
 else:
     print("Il n'y a pas de fichier Enregistrement.csv à lire pour moi :(")
--- plan.md
+++ plan.md
@@ -4,10 +4,5 @@
 SI LE MEC FAIT 1, true, false -> 1, true, false -> +1 série
 SI LE MEC FAIT 2, true, true -> +1 série
 
-je vais devoir
-  trier par SessionID
-  tier celle liste trié par date
-  check 
-
 
   2 true true ->1
